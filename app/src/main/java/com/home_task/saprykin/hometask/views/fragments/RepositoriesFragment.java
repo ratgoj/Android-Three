85d0e2a4c2dbfe3c7a84a2595a27ec7718d7aff4
Андрей Сапрыкин <ratgoj@gmail.com>
2018-10-09 21:51:13 +0300

    Добавлен EditText для поиска в репозитариях

--- app/src/main/java/com/home_task/saprykin/hometask/views/fragments/RepositoriesFragment.java
+++ app/src/main/java/com/home_task/saprykin/hometask/views/fragments/RepositoriesFragment.java
@@ -5,16 +5,22 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.arellomobile.mvp.MvpAppCompatFragment;
 import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.home_task.saprykin.hometask.R;
 import com.home_task.saprykin.hometask.presenters.RepoPresenter;
 import com.home_task.saprykin.hometask.presenters.interfaces.RepositoryVew;
+
+import io.reactivex.Observable;
+import io.reactivex.internal.operators.observable.ObservableJust;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +29,7 @@ public class RepositoriesFragment extends MvpAppCompatFragment implements Reposi
     private static final String REPO_TAG = "Repository";
 
     private View repoView;
+    private EditText serchRepo;
     private RecyclerView repoRecyclerView;
     private RecyclerView.LayoutManager repoLayoutManager;
 
@@ -38,12 +45,38 @@ public class RepositoriesFragment extends MvpAppCompatFragment implements Reposi
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         repoView = inflater.inflate(R.layout.fragment_repositories, container, false);
+
+        initView(repoView);
+
+        return repoView;
+    }
+
+    private void initView(View repoView){
         repoLayoutManager = new LinearLayoutManager(getActivity());
         repoRecyclerView = repoView.findViewById(R.id.repo_list);
         repoRecyclerView.setLayoutManager(repoLayoutManager);
         repoRecyclerView.setAdapter(presenter.getRepoRecyclerAdapter());
-        return repoView;
+
+        serchRepo = repoView.findViewById(R.id.repo_search);
+        TextWatcher searchWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+
     }
+
 
     @Override
     public void onRepoItemClick(int position) {
