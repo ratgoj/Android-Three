07d38341a07366915d826fe75f7f5a37cdf8fb91
Andrey Saprykin <a.saprykin@corp.101xp.com>
2018-10-19 16:50:01 +0300

    Создан собственный класс AppContext с инициализацией Realm

---
+++ app/src/main/java/com/home_task/saprykin/hometask/AppContext.java
@@ -0,0 +1,17 @@
+package com.home_task.saprykin.hometask;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class AppContext extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+        RealmConfiguration configuration =  new RealmConfiguration.Builder().build();
+        Realm.setDefaultConfiguration(configuration);
+    }
+}
