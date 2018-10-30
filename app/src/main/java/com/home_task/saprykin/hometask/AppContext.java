1c76a3b46c0d2c548f8b95d85cbfd9b7fbc02ca1
Андрей Сапрыкин <ratgoj@gmail.com>
2018-10-30 21:05:41 +0300

    Добавлен плагин fabric

--- app/src/main/java/com/home_task/saprykin/hometask/AppContext.java
+++ app/src/main/java/com/home_task/saprykin/hometask/AppContext.java
@@ -3,6 +3,8 @@ package com.home_task.saprykin.hometask;
 import android.app.Application;
 import android.content.SharedPreferences;
 
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
@@ -12,6 +14,7 @@ public class AppContext extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         Realm.init(this);
         RealmConfiguration configuration =  new RealmConfiguration.Builder().build();
         Realm.setDefaultConfiguration(configuration);
